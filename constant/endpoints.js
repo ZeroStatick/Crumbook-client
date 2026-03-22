6495b9578b6b1eab6d4c36b2a11ae23d6c041465
David Machiah <d.massiah99@gmail.com>
2026-03-22 10:48:14 +0200

    first commit added constant file and endpoints

---
+++ constant/endpoints.js
@@ -0,0 +1,8 @@
+export const BASE_URL = "http://localhost:3000"
+
+export const REGISTER_URL = "/api/auth/register"
+export const LOGIN_URL = "/api/auth/login"
+export const GET_ME_URL = "/api/users"
+export const RECIPE_URL = "api/recipes"
+export const RECIPE_BY_USER_URL = "api/recipes/user"
+export const INGREDIENT_URL = "api/ingredients"
