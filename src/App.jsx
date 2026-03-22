f104879392cafa101b491848d349dc862c4235db
Benel Kalski <benelkal1@gmail.com>
2026-03-22 12:51:40 +0200

    more basic stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./components/pages/Home.jsx"
+import { useEffect } from "react"
+import { GET_ME_URL } from "./constant/endpoints"
+import { useUserStore } from "./global/user"
+import axios from "axios"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import Register from "./components/Register"
+import Login from "./components/Login"
 
-// קומפוננטה = רכיב = פונקציה שמחזירה
-// JSX
+import { Toaster } from "react-hot-toast"
+
 function App() {
+  const { setUser, user } = useUserStore()
+  const checkUser = async () => {
+    const token = localStorage.getItem("token")
+    if (!token) return
+    try {
+      const { data } = await axios.get(GET_ME_URL)
+      setUser(data.user)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    checkUser()
+  }, [])
+
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/me" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <h1 className="text-3xl font-bold underline">404 not found</h1>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
