32335c6a22d702b0f3a0627f1e1ade74b9dc4499
David Machiah <d.massiah99@gmail.com>
2026-03-22 11:23:22 +0200

    added basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/pages/Home.jsx"
+
 // קומפוננטה = רכיב = פונקציה שמחזירה
 // JSX
 function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
