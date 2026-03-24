2355d27f1e9c4cac53065c033db9d530a709c26a
Benel Kalski <benelkal1@gmail.com>
2026-03-24 12:05:47 +0200

    sync  api

--- API/api.api.js
+++ API/api.api.js
@@ -1,9 +1,28 @@
 import axios from "axios"
 import { GET_ME_URL } from "../constant/endpoints"
 
+// Create an axios instance
+const api = axios.create({
+  baseURL: "", // Leave empty because we are using Vite proxy for /api
+})
+
+// Add a request interceptor to include the token in headers
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 export async function API_call(path, method = "GET", body = null) {
   try {
-    const response = await axios({
+    const response = await api({
       method,
       url: path,
       data: body,
@@ -15,10 +34,12 @@ export async function API_call(path, method = "GET", body = null) {
   }
 }
 
-export const get_all_users = () => axios.get(GET_ME_URL).then((res) => res.data)
+export const get_all_users = () => api.get(GET_ME_URL).then((res) => res.data)
 
 export const edit_user = (user_id, new_user_data) =>
-  axios.put(`${GET_ME_URL}/${user_id}`, new_user_data).then((res) => res.data)
+  api.put(`${GET_ME_URL}/${user_id}`, new_user_data).then((res) => res.data)
 
 export const delete_user = (user_id) =>
-  axios.delete(`${GET_ME_URL}/${user_id}`).then((res) => res.data)
+  api.delete(`${GET_ME_URL}/${user_id}`).then((res) => res.data)
+
+export default api
