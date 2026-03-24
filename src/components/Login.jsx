a08179cbac19cdd2155007985f7fb9066c8800f7
David Machiah <d.massiah99@gmail.com>
2026-03-24 09:40:32 +0200

    just adding login and register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,63 @@
-import React from "react"
+import { useState } from "react"
+import { BASE_URL, LOGIN_URL } from "../../constant/endpoints"
+import { NavLink, useNavigate } from "react-router-dom"
 
 const Login = () => {
-  return <div>Login</div>
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const navigate = useNavigate()
+
+  const formHandle = ({ target }) => {
+    setLoginData({
+      ...loginData,
+      [target.name]: target.value,
+    })
+  }
+
+  const loginSuccessSave = (token) => {
+    localStorage.setItem("token", token)
+    console.log("token saved")
+    navigate("/")
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post(`${BASE_URL}${LOGIN_URL}`, loginData)
+      const token = response.data.token
+
+      if (token) {
+        loginSuccessSave(token)
+      } else {
+        alert(response.data.message)
+      }
+    } catch (e) {
+      console.log("failed to log", e)
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formHandle}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={formHandle}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
 }
 
 export default Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,59 @@
-import React from "react"
+import { useState } from "react"
+import { BASE_URL, REGISTER_URL } from "../../constant/endpoints"
+import { NavLink, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const Register = () => {
-  return <div>Register</div>
+  const [registerData, setRegisterData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const navigate = useNavigate()
+  const updateData = ({ target }) => {
+    setRegisterData({
+      ...registerData,
+      [target.name]: target.value,
+    })
+  }
+  const handleRegister = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post(
+        `${BASE_URL}${REGISTER_URL}`,
+        registerData,
+      )
+      console.log(response.data)
+    } catch (e) {
+      console.log("failed to register", e.response.status)
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={updateData}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={updateData}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={updateData}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  )
 }
 
 export default Register
