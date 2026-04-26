dcd3acef895c294c5383d6388dc06f92861c48c6
Benel Kalski <benelkal1@gmail.com>
2026-04-26 12:05:37 +0300

    profile picture update

--- API/api.api.js
+++ API/api.api.js
@@ -47,8 +47,38 @@ export default api
 // User API helpers
 export const get_all_users = () => api.get("/api/users")
 
-export const edit_user = (user_id, new_user_data) =>
-  api.put(`/api/users/${user_id}`, new_user_data)
+export const edit_user = (user_id, new_user_data) => {
+  // Check if we need to send multipart/form-data (for file uploads)
+  const hasFile = new_user_data.profile_picture instanceof File
+
+  if (hasFile) {
+    const formData = new FormData()
+    Object.keys(new_user_data).forEach((key) => {
+      // If it's the file, append it
+      if (key === "profile_picture") {
+        if (new_user_data[key] instanceof File) {
+          formData.append(key, new_user_data[key])
+        }
+        // If it's a string URL, we skip it because we are uploading a new file
+        return
+      }
+      
+      // For other fields, only append if they have a value
+      if (new_user_data[key] !== undefined && new_user_data[key] !== null) {
+        formData.append(key, new_user_data[key])
+      }
+    })
+    return api.put(`/api/users/${user_id}`, formData)
+  }
+
+  // If no file, send as JSON but remove empty profile_picture to avoid overwriting
+  const dataToSend = { ...new_user_data }
+  if (!dataToSend.profile_picture) {
+    delete dataToSend.profile_picture
+  }
+
+  return api.put(`/api/users/${user_id}`, dataToSend)
+}
 
 export const delete_user = (user_id) => api.delete(`/api/users/${user_id}`)
 
